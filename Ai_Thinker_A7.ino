f55b530c80fd00767eb1c87df17b8adc08320e6a
dhahaj@gmail.com <dhahaj@gmail.com>
2019-10-21 22:38:19 -0500

    added secrets.h

--- Ai_Thinker_A7.ino
+++ Ai_Thinker_A7.ino
@@ -2,19 +2,19 @@ void setup() {
   // put your setup code here, to run once:
   Serial1.begin(115200);
   Serial.begin(115200);
+  while (!Serial);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial1.available() > 0) {
-    while (Serial1.available() > 0) {
+    while (Serial1.available() > 0)
       Serial.print((char)Serial1.read());
-    }
   }
   if (Serial.available() > 0) {
     while (Serial.available() > 0) {
       //Serial1.print((char)Serial.read());
-      char x = Serial.read();
+      char x = (char)Serial.read();
       if (x == '*')
         Serial1.print((char)26);
       else
